1ab7f9e553607e6cc686a6845e2a7d2d85973a20
Sarah Ney <sarah.ney@anthroware.com>
2019-05-31 10:24:57 -0400

    extract redundancy and prep for prod

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only:[:edit, :update, :show, :destroy]
   def index
     @articles = Article.all
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -22,11 +22,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Success!"
       redirect_to article_path(@article)
@@ -36,15 +34,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Articles was successfully deleted."
     redirect_to articles_path
   end
 
 
-  private def article_params
+  private
+   def article_params
       params.require(:article).permit(:title, :description)
-  end
+    end
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
 end
