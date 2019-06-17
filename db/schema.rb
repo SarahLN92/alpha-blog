374621cf9e8a8c2a69e4d0e7175e4678df330035
Sarah Ney <sarah.ney@anthroware.com>
2019-06-17 10:37:28 -0400

    user validation and such

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_134611) do
+ActiveRecord::Schema.define(version: 2019_06_06_142938) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_06_06_134611) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "image"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
