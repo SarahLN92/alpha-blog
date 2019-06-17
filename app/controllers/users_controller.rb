b1578c6ee3052d54e238f9a9028b775f2e742801
Sarah Ney <sarah.ney@anthroware.com>
2019-06-17 17:00:16 -0400

    displaying all the articles of each user on their profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user_params, only:[:edit, :update, :show, :destroy]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
       redirect_to articles_path
@@ -14,24 +19,32 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
     else
       render 'edit'
     end
+  end
+
+  def show
 
   end
+
+
+
 
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user_params
+    @user = User.find(params[:id])
   end
 
 end
